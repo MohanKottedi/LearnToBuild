a531aaa5e4e35ea39ea8a91b0114e48d6bd65ce6
mohan-kottedi <mohankottedi@gmail.com>
2026-05-01 10:51:35 +0530

    start from scratch learning to build big

---
+++ src/Task.java
@@ -0,0 +1,40 @@
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Task implements Comparable<Task>{
+    int id;
+    String name;
+    int priority;
+    LocalDateTime deadline;
+    boolean completed;
+    public Task(int id, String name, int priority, LocalDateTime deadline) {
+        this.id = id;
+        this.name = name;
+        this.priority = priority;
+        this.deadline = deadline;
+    }
+
+    public Task(String name) {
+        this.name = name;
+        this.priority = 4;
+        this.deadline = LocalDateTime.now().plusDays(6);
+    }
+    public int compareTo(Task a){
+        if(Math.abs(a.deadline.toLocalDate().toEpochDay()-this.deadline.toLocalDate().toEpochDay())<7){
+            return a.priority-this.priority;
+        }
+        else return a.deadline.getDayOfYear()-this.deadline.getDayOfYear();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id==task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/TaskManager.java
@@ -0,0 +1,49 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class TaskManager {
+    private ArrayList<Task> tasks=new ArrayList<>();
+    private int id=0;
+    public void add(String name, int priority, LocalDateTime deadline){
+        tasks.add(new Task(id++,name,priority,deadline));
+    }
+    public void delete(int id){
+        Iterator<Task> it=tasks.iterator();
+        while(it.hasNext()){
+            if(it.next().id==id) {it.remove();break;}
+        }
+
+    }
+    public void update(int id,String name,int priority,LocalDateTime deadline){
+        for(Task t:tasks){
+            if(t.id==id){
+                t.deadline=deadline;
+                t.priority=priority;
+                t.name=name;
+                break;
+            }
+        }
+    }
+    public void printAll(){
+        Collections.sort(tasks);
+        for(Task t:tasks) System.out.println(t.id+" "+t.name+" "+t.deadline+" "+t.priority);
+    }
+
+    public void markCompleted(int id){
+        for(Task t:tasks){
+            if(t.id==id) t.completed=true;
+        }
+    }
+    public void getCompleted(){
+        for(Task t:tasks)
+            if(t.completed)
+                System.out.println(t.id+" "+t.name+" "+t.deadline+" "+t.priority);
+    }
+    public void getPendings(){
+        for(Task t:tasks)
+            if(!t.completed)
+                System.out.println(t.id+" "+t.name+" "+t.deadline+" "+t.priority);
+    }
+}
